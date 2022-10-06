43155ce2dca92c87af981dd0913b8fcb6e6107eb
Sobhyz <asobhy540@gmail.com>
2022-10-06 23:38:38 +0200

    Done

--- OpenCV/bsg_AhmedSobhy_Task5.py
+++ OpenCV/bsg_AhmedSobhy_Task5.py
@@ -1,6 +1,7 @@
 from ctypes import pointer
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
 myPoints =[]
 global c
 c=0
@@ -9,6 +10,9 @@ def mouse(event,x,y,flag,params):
     if event == cv.EVENT_LBUTTONDOWN:
         myPoints.append([x,y])
         c+=1
+    if event == cv.EVENT_RBUTTONDOWN:
+        myPoints.clear()
+        c=0
 
 def reorder():
     x,y=0,0
@@ -19,19 +23,24 @@ def reorder():
             mx = i[0]+i[1]
             x,y = i[0],i[1]
     newPoints.append([x,y])
-    mx=1e9
-    tmpy,tmpx=y,x
+
+    mx=0
     for i in myPoints:
-        if abs(tmpy-i[1])<mx and (tmpx!=i[0] and tmpy!=i[1]):
-            mx=abs(tmpy-i[1])
-            x,y=i[0],i[1]
+        if i[0]-i[1]>mx:
+            mx = i[0]-i[1]
+            x,y = i[0],i[1]
     newPoints.append([x,y])
+
     mx=1e9
+
     for i in myPoints:
-        if abs(tmpx-i[0])<mx and (tmpx!=i[0] and tmpy!=i[1]):
-            mx=abs(tmpx-i[0])
-            x,y=i[0],i[1]
+        if i[0]-i[1]<mx:
+            mx = i[0]-i[1]
+            x,y = i[0],i[1]
     newPoints.append([x,y])
+    
+    
+
     mx=0
     for i in myPoints:
         if i[0]+i[1]>mx:
@@ -43,18 +52,23 @@ def reorder():
 
 
 img = cv.imread("/Users/hany/Downloads/tasks_images_/task_5/jhonsmith.jpg")
-width = img.shape[0]
-height = img.shape[1]
+width = img.shape[1]
+height = img.shape[0]
 
-while c<4:
-    print(c)
-    cv.imshow("original",img)
+while True:
+    copy = img.copy()
+    for i in myPoints:
+        cv.circle(copy, i, 5,(0,255,0),10)
+    cv.imshow("original",copy)
     cv.setMouseCallback("original", mouse)
-    cv.waitKey(1)
-myPoints = reorder()
-pts1 = np.float32(myPoints)
-pts2 = np.float32([[0, 0],[width, 0], [0, height],[width, height]])
-matrix = cv.getPerspectiveTransform(pts1, pts2)
-warp = cv.warpPerspective(img, matrix, (width, height))
-cv.imshow("warp", warp)
-cv.waitKey(0)
+    if c==4:
+        myPoints = reorder()
+        pts1 = np.float32(myPoints)
+        pts2 = np.float32([[0, 0],[width, 0], [0, height],[width, height]])
+        matrix = cv.getPerspectiveTransform(pts1, pts2)
+        warp = cv.warpPerspective(img, matrix, (width, height))
+        # warp = cv.cvtColor(warp, cv.COLOR_BGR2RGB)
+        cv.imshow("l",warp)
+    
+    if cv.waitKey(1)==ord('q'):
+        break
